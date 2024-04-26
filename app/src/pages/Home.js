0b903bf9a24a0b746d3e6f683af0bbe0d74ea27d
Stefan Ilic <SIlic@stu.naperville203.org>
2024-04-26 13:15:54 -0500

    start creation of table for glasses

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import MainHeader from '../components/MainHeader';
 import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
     const navigate = useNavigate();
+    
+    let categories = ["Pair Number, Is Available, Name, Date Out"];
+    const [data, setData] = useState(null);
+
     return (
         <div className="App">
             <MainHeader />
@@ -23,6 +28,23 @@ export default function Home() {
                 <button onClick={() => navigate("/out")} className="DashboardButton">
                     Check Out
                 </button>
+            </div>
+
+            <div style={{ display: "flex", 
+                        justifyContent: "center",
+                        alignItems: "center"}}>
+                <table style={{ width: "60%" }}>
+                    <tbody>
+                    <tr>
+                        {categories.map((item, index) => (
+                            <td key={index} style={{ border: "1px solid black", padding: "8px", textAlign: "center", width: "1000" }}>
+                                {item}
+                            </td>
+                        ))}
+                    </tr>
+                    
+                    </tbody>
+                </table>
             </div>
         </div>
     )
